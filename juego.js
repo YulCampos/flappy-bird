02bf4f2c102394d12fc6cecdbd9787b9e51521a9
Jorge Yul <jorgeyuldavilacampos@gmail.com>
2026-03-22 10:13:17 -0300

    Colisiones 21/03

--- juego.js
+++ juego.js
@@ -4,6 +4,7 @@ contexto.canvas.height = 530
 
 //VARIABLES
 
+var score = 0
 var FPS = 60
 var gravedad = 1.5
 var personaje = {
@@ -67,10 +68,17 @@ function loop(){
                 y: Math.floor(Math.random()*tuberiaNorte.height) - tuberiaNorte.height
             })
         }
+        //COLISIONES
+        if (personaje.x + bird.width>=tuberias[i].x && personaje.x <= tuberias[i].x + tuberiaNorte.width && (personaje.y <=tuberias[i].y + tuberiaNorte.height || personaje.y + bird.height >= tuberias[i].y + constante)||personaje.y + bird.height>= contexto.canvas.height - suelo.height){
+            location.reload()
+        }
 
     }
     //CONDICIONES
     personaje.y += gravedad 
+    contexto.fillStyle = "rgba(0,0,0,1)"
+    contexto.fond = "25px Arial"
+    contexto.fillText("Score : "+ score,10,contexto.canvas.height-40)
 }   
 //EVENTOS
 window.addEventListener("keydown",presionar)
